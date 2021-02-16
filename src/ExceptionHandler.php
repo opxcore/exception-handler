0a1b5856977cfd0fcc30b1b8f82a7ca69e629543
lozovoyv <lozovoyv@gmail.com>
2021-02-16 17:14:58 +0300

    replace ErrorException with own

--- src/ExceptionHandler.php
+++ src/ExceptionHandler.php
@@ -10,8 +10,20 @@
 
 namespace OpxCore\ExceptionHandler;
 
-use ErrorException;
+use OpxCore\ExceptionHandler\Exceptions\ErrorException;
 use OpxCore\App\Interfaces\AppInterface;
+use OpxCore\ExceptionHandler\Formatter\FormatterInterface;
+use OpxCore\ExceptionHandler\Formatter\Console\Formatter as ConsoleFormatter;
+use OpxCore\ExceptionHandler\Formatter\Html\Formatter as HtmlFormatter;
+use OpxCore\ExceptionHandler\Formatter\Json\Formatter as JsonFormatter;
+use OpxCore\ExceptionHandler\Formatter\Soap\Formatter as SoapFormatter;
+use OpxCore\ExceptionHandler\Formatter\Xml\Formatter as XmlFormatter;
+use OpxCore\ExceptionHandler\Output\Console\Output as ConsoleOutput;
+use OpxCore\ExceptionHandler\Output\Html\Output as HtmlOutput;
+use OpxCore\ExceptionHandler\Output\Json\Output as JsonOutput;
+use OpxCore\ExceptionHandler\Output\OutputInterface;
+use OpxCore\ExceptionHandler\Output\Soap\Output as SoapOutput;
+use OpxCore\ExceptionHandler\Output\Xml\Output as XmlOutput;
 use OpxCore\ExceptionHandler\Interfaces\ExceptionHandlerInterface;
 use Throwable;
 
@@ -54,45 +66,47 @@ class ExceptionHandler implements ExceptionHandlerInterface
      */
     public function handle(Throwable $e): void
     {
+        // TODO processing
+
+        $this->render($e);
+    }
+
+    /**
+     * Render throwable for application selected output.
+     *
+     * @param Throwable $e
+     */
+    protected function render(Throwable $e): void
+    {
         switch ($this->app->outputMode()) {
+            case AppInterface::APP_OUTPUT_XML:
+                $formatter = new ConsoleFormatter();
+                $output = new ConsoleOutput();
+                break;
+            case AppInterface::APP_OUTPUT_SOAP:
+                $formatter = new HtmlFormatter();
+                $output = new HtmlOutput();
+                break;
             case AppInterface::APP_OUTPUT_CONSOLE:
-                $this->renderForConsole($e);
+                $formatter = new JsonFormatter();
+                $output = new JsonOutput();
                 break;
             case AppInterface::APP_OUTPUT_JSON:
-                $this->renderForJson($e);
+                $formatter = new SoapFormatter();
+                $output = new SoapOutput();
                 break;
             case AppInterface::APP_OUTPUT_HTTP:
             default:
-                $this->renderForHttp($e);
+                $formatter = new XmlFormatter();
+                $output = new XmlOutput();
                 break;
         }
-    }
 
-    /**
-     * Render throwable for console output.
-     *
-     * @param Throwable $e
-     */
-    protected function renderForConsole(Throwable $e): void
-    {
-    }
+        /** @var FormatterInterface $content */
+        $content = $formatter->format($e);
 
-    /**
-     * Render throwable for json output.
-     *
-     * @param Throwable $e
-     */
-    protected function renderForJson(Throwable $e): void
-    {
-    }
-
-    /**
-     * Render throwable for http output.
-     *
-     * @param Throwable $e
-     */
-    protected function renderForHttp(Throwable $e): void
-    {
+        /** @var OutputInterface $output */
+        $output->output($content);
     }
 
     /**
